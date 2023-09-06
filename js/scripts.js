0be9a8e5195aa57ac86b5abe97a40e19c552b34e
Jorgec88 <j.carralero88@gmail.com>
2023-09-06 15:50:58 -0700

    Add pokemonList info
    Add pokemon info for a pokedex project

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,38 @@
-alert('hello world')
+let pokemonList = [
+{ 
+    name: 'Bulbasaur',
+    type: ['grass' , 'poison'],
+    height: 2.04,
+    weight: 15.2,
+    category: 'seed'
+},
+{
+    name: 'Charizard',
+    type: ['fire' , 'flying'],
+    height: 5.07,
+    weight: 199.5,
+    category: 'flame'
+},
+{
+    name: 'Alakazam',
+    type: ['psychic'],
+    height: 4.11,
+    weight: 105.8,
+    category: 'psi'
+},
+{
+    name: 'Articuno',
+    type: ['ice' , 'flying'],
+    height: 5.07,
+    weight: 122.1,
+    category: 'freeze'
+},
+{
+    name: 'Dragonite',
+    type: ['dragon' , 'flying'],
+    height: 7.03,
+    weight: 463,
+    category: 'dragon'
+}
 
-let favoriteFood = 'pizza'
-document.write(favoriteFood)
+]
